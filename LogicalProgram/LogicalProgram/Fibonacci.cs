593ceaff764cab08ee63dce225a218b45aabbf90
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-06 08:26:47 +0530

    Fibonaci Series

---
+++ LogicalProgram/LogicalProgram/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Fiboncci
+    {
+        public void Series()
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int num1 = 0, num2 = 1, num3=0;
+
+            for (int i = 0; i < num; i++)
+            {
+                num1 = num2;
+                num2 = num3;
+                num3 = num1 + num2;
+                Console.WriteLine(num3);
+
+                
+            }
+        }
+    }
+}
---
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Logical
+    {
+        static void Main(string[] args)
+        {
+            Fiboncci fiboncci= new Fiboncci();
+            fiboncci.Series();
+
+
+        }
+    }
+}
